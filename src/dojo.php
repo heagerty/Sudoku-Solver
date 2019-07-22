c86c8db48f1cdb7d18b2a5538efb9be6c5ede876
Casey Heagerty <cheagerty@gmail.com>
2019-07-22 11:38:33 +0200

    add dojo file for reference

---
+++ src/dojo.php
@@ -0,0 +1,103 @@
+<?php
+
+
+namespace App;
+
+
+use foo\bar;
+
+/**
+ * Class SudokuChecker
+ * @package App
+ */
+class SudokuChecker
+{
+    /**
+     * @var array
+     */
+    private $grid;
+
+    /**
+     * @var array
+     */
+    private $range;
+
+    /**
+     * @var array
+     */
+    private $parts = [];
+
+    /**
+     * @var bool
+     */
+    private $validity = true;
+
+    /**
+     * SudokuChecker constructor.
+     * @param array $grid
+     */
+    public function __construct(array $grid)
+    {
+        $this->grid = $grid;
+        $this->range = range(1, count($grid));
+        $this->getLines();
+        $this->getCols();
+        $this->getSquares();
+    }
+
+    /**
+     * @return bool
+     */
+    public function check(): bool
+    {
+        return $this->checkParts();
+    }
+
+    private function getLines(): void
+    {
+        $this->parts = $this->grid;
+    }
+
+    private function getCols(): void
+    {
+        for ($i = 0; $i < count($this->grid); $i++) {
+            $col = [];
+            for ($j = 0; $j < count($this->grid); $j++) {
+                $col[] = $this->grid[$j][$i];
+            }
+            $this->parts[] = $col;
+        }
+    }
+
+    private function getSquares(): void
+    {
+        $squareSize = sqrt(count($this->grid));
+        for ($i = 0; $i < count($this->grid); $i += $squareSize) {
+            for ($j = 0; $j < count($this->grid); $j += $squareSize) {
+                $square = [];
+                for ($k = $i; $k < $i + $squareSize; $k++) {
+                    for ($l = $j; $l < $j + $squareSize; $l++) {
+                        $square[] = $this->grid[$k][$l];
+                    }
+                }
+                $this->parts[] = $square;
+            }
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    private function checkParts(): bool
+    {
+        foreach ($this->parts as $part) {
+            sort($part);
+            if ($part !== $this->range) {
+                $this->validity = false;
+                break;
+            }
+        }
+
+        return $this->validity;
+    }
+}
