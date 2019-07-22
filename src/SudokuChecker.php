bed298c647f72739474ca0cb76965655ce91b482
Casey Heagerty <cheagerty@gmail.com>
2019-07-22 15:56:49 +0200

    add Checker to repo

---
+++ src/SudokuChecker.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App;
+
+class SudokuChecker
+
+{
+
+
+    public function sudokuChecker(array $coordinates)
+    {
+
+        $size = 9;
+        $numbersUsed = range(1, 9);
+
+
+        for ($i=0; $i<$size; $i++) {   //pass over rows
+            $row = [];
+            for ($j = 0; $j < $size; $j++) {   //check each row
+                if (in_array($coordinates[$i][$j], $numbersUsed)) {
+                    $row[] = $coordinates[$i][$j];
+                } else {
+                    echo '$i=' . $i . '  $j=' . $j;
+                    return false;
+                }
+            }
+            $uniqueRow = array_unique($row);
+            if (sizeof($uniqueRow) !== $size) {
+                return false;
+            }
+
+
+            $column = [];
+            for ($j = 0; $j < $size; $j++) {   //check each column
+                $column[] = $coordinates[$j][$i];
+            }
+
+            $uniqueColumn = array_unique($column);
+            if (sizeof($uniqueColumn) !== $size) {
+                return false;
+            }
+
+        }
+
+
+
+        //check boxes
+        if (sqrt($size) == floor(sqrt($size))) {   //make sure it's divisible in
+
+            echo "SQUARE TEST";
+
+
+            $boxSize = sqrt($size);
+
+            echo $boxSize;
+
+
+
+            for ($i = 0; $i < $boxSize; $i++) {              //each row
+                for ($j = 0; $j < $boxSize; $j++) {            //each column
+
+
+                    $box = [];
+
+                    for ($k = 0 + $boxSize*$i; $k < $boxSize + $boxSize*$i; $k++) {              //each box
+                        for ($l = 0 + $boxSize*$j; $l < $boxSize + $boxSize*$j; $l++) {
+
+                            $box[] = $coordinates[$k][$l];
+
+
+
+                        }
+                    }
+                    $uniqueBox = array_unique($box);
+                    if (count($uniqueBox) !== $size) {
+                        return false;
+                    }
+
+
+                }
+
+
+            }
+        }
+
+
+
+    return true;
+
+    }
+
+
+
+}
--- src/dojo.php
+++ src/dojo.php
@@ -4,7 +4,7 @@
 namespace App;
 
 
-use foo\bar;
+//use foo\bar;
 
 /**
  * Class SudokuChecker
